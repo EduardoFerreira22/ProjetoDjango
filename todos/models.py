d0e6bb80934ec841c93ea78256acf388d619226c
EduardoFerreira22 <eduardoferreira_of@outlook.com>
2023-10-16 09:24:22 -0300

    Novas alterações

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+# criando os atributos para criar uma tabela no db.sqlite3
+class Todos(models.Model):
+  title = models.CharField(max_length=100, null=False, blank=False) # cria um tipo de dados e define a quantidade de caracteres que esse tipo de dados pode receber BLANK "não permite que seja cadastrados caracteres em branco"
+  created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+  deadline = models.DateTimeField(null=False, blank=False)
+  Finished_at = models.DateTimeField(null=True)
--- todos/views.py
+++ todos/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-  return HttpResponse("Helo Web!")
+    # return HttpResponse('<h1>Helo Web!</h1>')
+    return render(request, "todos/home.html") #A função render renderiza templates html, caso vc já tenha um criado
 
-# Create your views here.
+  # Create your views here.
