91789a4989134a53c67b32bf71529a3f52c9a79f
EduardoFerreira22 <eduardoferreira_of@outlook.com>
2023-10-17 16:03:22 -0300

    Iniciando o curso de Django Udemy

--- todos/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def home(request):
-    # return HttpResponse('<h1>Helo Web!</h1>')
-    return render(request, "todos/home.html") #A função render renderiza templates html, caso vc já tenha um criado
-
-  # Create your views here.
